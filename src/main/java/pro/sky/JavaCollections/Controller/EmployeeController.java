1766ed57fa2d8fd1a4983d7b20b9852dd8d1002e
kila1 <kila14081996@gmail.com>
2024-05-20 23:12:16 +0300

    дз с коллекциями 1 переделка с помощью видео разбора

--- src/main/java/pro/sky/JavaCollections/Controller/EmployeeController.java
+++ src/main/java/pro/sky/JavaCollections/Controller/EmployeeController.java
@@ -1,22 +1,19 @@
 package pro.sky.JavaCollections.Controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 import pro.sky.JavaCollections.Employee;
 import pro.sky.JavaCollections.EmployeeService;
-import pro.sky.JavaCollections.Exceptions.ArrayIsFullException;
-import pro.sky.JavaCollections.Exceptions.EmployeeAlreadyAddedException;
-import pro.sky.JavaCollections.Exceptions.EmployeeNotFoundException;
+
+import java.util.Collection;
 
 @RestController
-@RequestMapping
+@RequestMapping("/employee")
 public class EmployeeController {
 
-    public EmployeeService employeeService;
+    private final EmployeeService employeeService;
 
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
@@ -25,35 +22,21 @@ public class EmployeeController {
 
     @GetMapping("/employee/add")
     public Employee addEmployee(@RequestParam String firstName, @RequestParam String lastName) {
-        Employee employee = new Employee(firstName, lastName);
-        try {
-            employeeService.addEmployee(firstName,lastName);
-            return employee;
-        } catch (ArrayIsFullException e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Array is full");
-        } catch (EmployeeAlreadyAddedException e) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT, "Employee already added");
-        }
+        return employeeService.addEmployee(firstName, lastName);
     }
 
     @GetMapping("/employee/remove")
     public Employee removeEmployee(@RequestParam String firstName, @RequestParam String lastName) {
-        Employee employee = new Employee(firstName, lastName);
-        try {
-            employeeService.removeEmployee(firstName,lastName);
-            return employee;
-        } catch (EmployeeNotFoundException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee not found");
-        }
+        return employeeService.removeEmployee(firstName, lastName);
     }
 
     @GetMapping("/employee/search")
     public Employee searchEmployee(@RequestParam String firstName, @RequestParam String lastName) {
-        Employee employee = new Employee(firstName, lastName);
-        if (employeeService.searchEmployee(firstName, lastName)) {
-            return employee;
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee not found");
-        }
+        return employeeService.searchEmployee(firstName, lastName);
+    }
+
+    @GetMapping("/employee")
+    public Collection<Employee> searchAllEmployee() {
+        return employeeService.searchAllEmployee();
     }
 }
--- src/main/java/pro/sky/JavaCollections/Exceptions/EmployeeNotFoundException.java
+++ src/main/java/pro/sky/JavaCollections/Exceptions/EmployeeNotFoundException.java
@@ -1,7 +1,10 @@
 package pro.sky.JavaCollections.Exceptions;
 
-import java.io.IOException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.io.IOException;
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class EmployeeNotFoundException extends RuntimeException {
     public EmployeeNotFoundException(String message) {
         super(message);
